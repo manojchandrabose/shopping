f7970703ee08c5936c5bcdd655f958ef0fd34088
Manoj Prabaharan <manoj.magento@gmail.com>
2020-05-01 22:25:02 +0530

    Updates

--- precache-manifest.769331687b6982344a9decf5d462df19.js
+++ precache-manifest.769331687b6982344a9decf5d462df19.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/shopping/static/js/runtime~main.c4c43f3f.js"
   },
   {
-    "revision": "688a7271a1457ab3397a",
-    "url": "/shopping/static/js/main.688a7271.chunk.js"
+    "revision": "5339a0bc6231c8bee07a",
+    "url": "/shopping/static/js/main.5339a0bc.chunk.js"
   },
   {
     "revision": "3b02b2f19e67b2fc72d4",
     "url": "/shopping/static/js/1.3b02b2f1.chunk.js"
   },
   {
-    "revision": "688a7271a1457ab3397a",
+    "revision": "5339a0bc6231c8bee07a",
     "url": "/shopping/static/css/main.182e4bff.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/shopping/static/css/1.0c93c511.chunk.css"
   },
   {
-    "revision": "8b76b90a1633d952e20c39940fab9dc4",
+    "revision": "9a84b4d0026ef3158ce97e270eba6ce6",
     "url": "/shopping/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shopping/precache-manifest.f089a83f6bd1421a151fdda9c1f4b2dd.js"
+  "/shopping/precache-manifest.769331687b6982344a9decf5d462df19.js"
 );
 
 workbox.clientsClaim();
